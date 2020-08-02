58f670ff3d76c07bdf8eb78c4341e325f8df44d7
venusoftwares <venu.softwares@gmail.com>
2020-08-02 09:50:59 +0530

    added project

--- gooesy/MainActivity.cs
+++ gooesy/MainActivity.cs
@@ -8,12 +8,14 @@ using Android.Widget;
 using Android.OS;
 using gooesy.Views;
 using gooesy.Models;
+using Android.Graphics;
 
 namespace gooesy
 {
-    [Activity(Label = "gooesy", MainLauncher = true)]
+    [Activity(Label = "gooesy", MainLauncher = true, Theme = "@android:style/Theme.NoTitleBar")]
     public class MainActivity : Activity
     {
+        WebView webView;
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
@@ -21,57 +23,56 @@ namespace gooesy
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
 
-            var webView = FindViewById<WebView>(Resource.Id.webView);
+            webView = FindViewById<WebView>(Resource.Id.webView);
             webView.Settings.JavaScriptEnabled = true;
+            webView.SetWebViewClient(new HelloWebViewClient());
+            webView.LoadUrl("https://www.gooesy.com/services-list");
 
-            // Use subclassed WebViewClient to intercept hybrid native calls
-            webView.SetWebViewClient(new HybridWebViewClient());
 
-            // Render the view from the type generated from RazorView.cshtml
-            var model = new Model1() { Text = "Text goes here" };
-            var template = new RazorView() { Model = model };
-            var page = template.GenerateString();
-
-            // Load the rendered HTML into the view with a base URL 
-            // that points to the root of the bundled Assets folder
-            webView.LoadDataWithBaseURL("file:///android_asset/", page, "text/html", "UTF-8", null);
-
+            base.OnCreate(bundle); 
+            
         }
-
-        private class HybridWebViewClient : WebViewClient
+        public class HelloWebViewClient : WebViewClient
         {
-            public override bool ShouldOverrideUrlLoading(WebView webView, string url)
+            public override bool ShouldOverrideUrlLoading(WebView view, IWebResourceRequest request)
             {
+                view.LoadUrl(request.Url.ToString());
+                return false;
+            }
+            public override void OnPageStarted(WebView view, string url, Bitmap favicon)
+            {
+                base.OnPageStarted(view, url, favicon);
+            }
 
-                // If the URL is not our own custom scheme, just let the webView load the URL as usual
-                var scheme = "hybrid:";
+            // For API level 24 and later
+           
 
-                if (!url.StartsWith(scheme))
-                    return false;
-
-                // This handler will treat everything between the protocol and "?"
-                // as the method name.  The querystring has all of the parameters.
-                var resources = url.Substring(scheme.Length).Split('?');
-                var method = resources[0];
-                var parameters = System.Web.HttpUtility.ParseQueryString(resources[1]);
-
-                if (method == "UpdateLabel")
-                {
-                    var textbox = parameters["textbox"];
-
-                    // Add some text to our string here so that we know something
-                    // happened on the native part of the round trip.
-                    var prepended = string.Format("C# says \"{0}\"", textbox);
-
-                    // Build some javascript using the C#-modified result
-                    var js = string.Format("SetLabelText('{0}');", prepended);
-
-                    webView.LoadUrl("javascript:" + js);
-                }
-
-                return true;
+            public override void OnReceivedError (WebView view, IWebResourceRequest request, WebResourceError error )
+            {
+                 
+                base.OnReceivedError(view, request, error );
+                
+                //------------------------------
+                // In this line, I want to redirect my page
+                 
+            }
+            public override void OnPageFinished(WebView view, string url)
+            {
+                base.OnPageFinished(view, url);
             }
         }
-    }
-}
+        public override bool OnKeyDown(Keycode keyCode,KeyEvent e)
+        {
+            if (keyCode == Keycode.Back && webView.CanGoBack())
+            {
+                webView.GoBack();
+                return true;
+            }
+            return base.OnKeyDown(keyCode, e);
+        }
+       
 
+    }
+
+ 
+}
